df5bb497de7346ee165eb80e4f78030149f59b61
wolfpirker <w_pirker@gmx.de>
2022-01-08 22:42:08 +0100

    Express - Params and querystrings

--- server.js
+++ server.js
@@ -6,12 +6,33 @@ app.get('/', (req, res) => {
     res.send('<html><body><h1 style="background:red">Hello there</h1></body></html>')
 })
 
-app.get('/api/user', (req, res) => {
-    res.send({
-        name: 'francis',
-        lastname: 'jones'
-    });
+
+app.get('/api/:username/:id', (req, res) => {
+    let id = req.params.id;
+    let username = req.params.username;
+    res.send(`<html>
+        <body>
+            <h1 style="background:red">The user id is ${id} and the username is ${username}</h1>
+        </body>
+    </html>`)
+    // res.send({
+    //     name:'francis',
+    //     lastname:'jones'
+    // });
 })
+
+
+app.get('/api/car', (req, res) => {
+    // http://localhost:3000/api/car?brand=ford&model=fiesta
+    let brand = req.query.brand;
+    let model = req.query.model;
+
+    res.send({
+        brand,
+        model
+    })
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port);
